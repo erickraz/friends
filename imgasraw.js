6eff64a330f210d359dce8f2ba4c229b77a03ae4
Shang-Pu Fan <poo@Shang-PudeMacBook-Pro.local>
2016-02-18 19:34:34 -0500

    refinement

--- imgasraw.js
+++ imgasraw.js
@@ -15,3 +15,21 @@ function imgAsRaw(img, height, width)
   var pixels = ctx.getImageData(0,0,canvas.width,canvas.height).data;
   return pixels;
 }
+function loadImages(sources, callback) {
+  var images = [];
+  var loadedImages = 0;
+  var numImages = 0;
+  // get num of sources
+  for(var src in sources) {
+    numImages++;
+  }
+  for(var i = 0; i < sources.length; ++i) {
+    images[i] = new Image();
+    images[i].crossOrigin = "Anonymous";
+    images[i].onload = function() {
+      if(++loadedImages >= numImages)
+        callback(images);
+    };
+    images[i].src = sources[i];
+  }
+}
